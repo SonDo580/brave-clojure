d352de83e10f42b490b9264a8c2d45c67a76130a
Son Do <dominhson580@gmail.com>
2024-06-08 22:19:13 +0700

    Chapter 7 notes

---
+++ 07_read_and_eval/note.clj
@@ -0,0 +1,29 @@
+;; MACRO
+
+(defmacro ignore-last-operand
+  [function-call]
+  (butlast function-call))
+
+(ignore-last-operand (+ 1 2 10)) ; 3
+(macroexpand '(ignore-last-operand (+ 1 2 10))) ; (+ 1 2)
+
+(defmacro infix
+  [infixed]
+  (list (second infixed)
+        (first infixed)
+        (last infixed)))
+
+(infix (1 + 2)) ; 3
+
+;; SYNTAX ABSTRACTION
+(defn read-resource
+  "Read a resource into a string"
+  [path]
+  (read-string (slurp (clojure.java.io/resource path))))
+
+(defn read-resource-2
+  [path]
+  (-> path
+      clojure.java.io/resource
+      slurp
+      read-string))
